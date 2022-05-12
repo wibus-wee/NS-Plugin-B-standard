41f2429c28b6ea2c154cf2423d06a0bbde735a44
wibus-wee <1596355173@qq.com>
2022-05-12 23:31:28 +0800

    init project

---
+++ gs.config.js
@@ -0,0 +1,20 @@
+module.export = {
+  "interface": {
+
+    // 输出部分
+    "posts/output": [{}],
+    "pages/output": [{}],
+    "global/output": [{}],
+
+    // 输入部分
+    "posts/input": [{}],
+    "pages/input": [{}],
+    "global/input": [{}],
+
+    // 数据库操作
+    "database": [{}],
+
+    // 请求时
+    "request": [{}],
+  }
+}
